404172a00d95ae3cbd411140d6fdc1224b19333a
abhishek <vishnoi.abhishek@hotmail.com>
2022-07-01 20:59:42 +0530

    add kafka route

--- src/main/java/org/mycompany/MyRoute.java
+++ src/main/java/org/mycompany/MyRoute.java
@@ -47,11 +47,11 @@ public class MyRoute extends RouteBuilder {
                     exchange.getIn().setBody(exchange.getIn().getHeader("log" ,String.class));
                 }
             })
-			 .to("kafka:myTopic?brokers=samsung-cluster-kafka-brokers:9092");
+			 .to("kafka:samsung-topic?brokers=samsung-cluster-kafka-bootstrap:9092");
 
 
 		 // Kafka Consumer
-		 from("kafka:myTopic?brokers=samsung-cluster-kafka-brokers:9092")
+		 from("kafka:samsung-topic?brokers=samsung-cluster-kafka-bootstrap:9092")
 				 .log("Message received from Kafka : ${body}")
 				 .log("    on the topic ${headers[kafka.TOPIC]}")
 				 .log("    on the partition ${headers[kafka.PARTITION]}")
