91b63ea08823c0a22f2bb0f51c6164095b4e3b49
abhishek <vishnoi.abhishek@hotmail.com>
2022-07-01 20:07:13 +0530

    add kafka route

--- src/main/java/org/mycompany/MyRoute.java
+++ src/main/java/org/mycompany/MyRoute.java
@@ -1,5 +1,7 @@
 package org.mycompany;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
@@ -7,49 +9,60 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRoute extends RouteBuilder {
 
-    @Override
-    public void configure() throws Exception {
+	 @Override
+	    public void configure() throws Exception {
 
 
-        restConfiguration()
-                .enableCORS(true)
-                .component("jetty")
-                .host("0.0.0.0")
-                .port(8988)
-                .bindingMode(RestBindingMode.json);
+	    restConfiguration()
+         .enableCORS(true)
+         .component("jetty")
+         .host("0.0.0.0")
+         .port(8988)
+         .bindingMode(RestBindingMode.json);
 
 
-        rest()
-                .get("/hello")
-                .to("direct:hello")
-                .get("/hello-kafka")
-                .to("direct:hello-kafka");
+
+      rest()
+         .get("/hello")
+         .to("direct:hello")
+			  .get("/hello-kafka")
+			  .to("direct:hello-kafka");
 
 
-        from("direct:hello")
-                .routeId("GreetingRoute")
-                .to("direct:greetStranger");
+     from("direct:hello")
+     .routeId("GreetingRoute")
+     	 .to("direct:greetStranger");
 
 
-        from("direct:greetStranger")
-                .routeId("greetStranger")
-                .setBody(simple("Hello Application!"));
+     from("direct:greetStranger")
+	  	.routeId("greetStranger")
+	  	.setBody(simple("Hello Application!"));
 
 
-        from("direct:hello-kafka")
-                .routeId("KafkaGreetingRoute")
-                .to("kafka:myTopic?brokers=my-cluster-kafka-brokers:9092");
+	from("direct:hello-kafka")
+			.routeId("KafkaGreetingRoute")
+            .process(new Processor() {
+                @Override
+                public void process(Exchange exchange) throws Exception {
+                    exchange.getIn().setBody(exchange.getIn().getHeader("log" ,String.class));
+                }
+            })
+			 .to("kafka:myTopic?brokers=my-cluster-kafka-brokers:9092");
 
 
-        // Kafka Consumer
-        from("kafka:myTopic?brokers=my-cluster-kafka-brokers:9092")
-                .log("Message received from Kafka : ${body}")
-                .log("    on the topic ${headers[kafka.TOPIC]}")
-                .log("    on the partition ${headers[kafka.PARTITION]}")
-                .log("    with the offset ${headers[kafka.OFFSET]}")
-                .log("    with the key ${headers[kafka.KEY]}");
+		 // Kafka Consumer
+		 from("kafka:myTopic?brokers=my-cluster-kafka-brokers:9092")
+				 .log("Message received from Kafka : ${body}")
+				 .log("    on the topic ${headers[kafka.TOPIC]}")
+				 .log("    on the partition ${headers[kafka.PARTITION]}")
+				 .log("    with the offset ${headers[kafka.OFFSET]}")
+				 .log("    with the key ${headers[kafka.KEY]}");
 
 
-    }
+
+
+
+
+	 }
 
 }
